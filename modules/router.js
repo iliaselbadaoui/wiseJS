8a461e482232711e7f3467f6bcdbd1d390f42c7a
Ilias Elbadaoui <iliassovich@outlook.com>
2022-01-14 23:07:22 +0100

    router still sick

--- modules/router.js
+++ modules/router.js
@@ -1,14 +1,10 @@
-import { isMemberExpressionBrowser } from "@vue/compiler-core";
-
-function initiate(mainRoute, mainComponent) {
+function initiate(mainRoute, mainComponent)
+{
 	
 	history.pushState(JSON.stringify(mainComponent), "", mainRoute);
 	window.onpopstate = function () {
 		alert(history.state);
 	}
-	// window.onbeforeunload = function () {
-	// 	alert("It's being unloaded")
-	// }
 }
 
 export default { initiate }
